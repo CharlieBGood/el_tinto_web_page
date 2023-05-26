5e6a2da8d0606277b5db294f72e83c74d5e50e96
CharlieBGood <carlosbueno1196@gmail.com>
2023-05-26 17:36:09 -0500

    change alignment

--- src/components/organisms/TodaysTinto/index.tsx
+++ src/components/organisms/TodaysTinto/index.tsx
@@ -33,7 +33,7 @@ const TodaysTinto = () => {
 
     useEffect(() => {
         if(tintoContent !== null){
-            document.getElementById(hash.substring(1))?.scrollIntoView({block: "start", behavior: 'auto'});
+            document.getElementById(hash.substring(1))?.scrollIntoView({block: "end", behavior: 'auto'});
         }
     }, [tintoContent])
 
