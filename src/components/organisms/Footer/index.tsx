f61360bfcfe763a420f041e300c450ab5496bd11
CharlieBGood <carlosbueno1196@gmail.com>
2023-10-04 17:35:32 -0500

    change footer

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,7 +1,7 @@
 import { FlexContainer } from "../../atoms";
 import THEME from '../../../utils/styledTheme';
 import styled from "styled-components";
-import { Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 
 const FooterLine = styled.hr`
     background-color: #FFFFFF;
@@ -16,13 +16,30 @@ const Footer = () => {
             <FlexContainer width="90%" direction="column" padding="10px 0" alignItems="center">
                 <FooterLine />
                 <Typography variant="subtitle2" textAlign='center'>
-                    Copyright © El Tinto 2023 Todos los derechos reservados.
-                </Typography>
-                <Typography variant="subtitle2" textAlign='center' margin='20px 0 0 0'>
-                    ¿Tiene alguna sugerencia o quisiera que incluyeramos algo nuevo?
+                    <span style={{fontWeight: "700"}}>Pase y charlemos en La Cafetería</span>, la comunidad de El Tinto, <Link style={{color: "#FFF", textDecoration: "underline"}} href="https://discord.gg/BT5TDrMd8F">aquí</Link>.
                 </Typography>
                 <Typography variant="subtitle2" textAlign='center'>
-                    Escríbanos a <a href='mailto:info@eltinto.xyz' style={{color: '#FFFFFF'}}>info@eltinto.xyz</a> y le daremos respuesta antes de que se le enfríe el tintico.
+                    También puede seguirnos en:
+                </Typography>
+                <FlexContainer>
+                    <Link href="https://twitter.com/ElTinto_" padding="10px">
+                        <img 
+                            src="/images/utils/white_twitter.png" alt="twitter"
+                            style={{maxWidth: "20px"}}
+                        />
+                    </Link>
+                    <Link href="https://twitter.com/ElTinto_" padding="10px">
+                        <img 
+                            src="/images/utils/white_instagram.png" alt="twitter"
+                            style={{maxWidth: "20px"}}
+                        />
+                    </Link>
+                </FlexContainer>
+                <Typography variant="subtitle2" textAlign='center' margin='20px 0 0 0'>
+                    <span style={{fontWeight: "700"}}>¿Tiene alguna sugerencia?</span> Escríbanos a <a href='mailto:info@eltinto.xyz' style={{color: '#FFFFFF'}}>info@eltinto.xyz</a>.
+                </Typography>
+                <Typography variant="subtitle2" textAlign='center' marginTop="20px">
+                    Copyright © 2023 El Tinto. Todos los derechos reservados.
                 </Typography>
                 <FooterLine />
             </FlexContainer>
